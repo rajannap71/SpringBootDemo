630c07b6c695b05456b6f017fb418f91b4ceb46a
rajanna.p <prgarani@gmail.com>
2020-06-28 16:13:46 +0530

    second commit

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Main {
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "Hello World!!!";
 	}
 	  public static void main(String[] args) {
 	        SpringApplication.run(Main.class, args);
